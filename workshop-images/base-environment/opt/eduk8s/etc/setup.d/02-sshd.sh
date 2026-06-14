e5d3b55274bd6b4a355146256f43a064a1b4cd9e
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2026-06-14 12:44:22 +1000

    Harden the workshop session sshd configuration.
    Lowers the sshd log level from DEBUG3 (which the OpenSSH documentation notes violates user privacy) to INFO, removes generation of the unused DSA host key (DSA was removed in OpenSSH 9.8 so the command failed on current images anyway), and tightens the configuration: public-key authentication is required explicitly, root login is disallowed, agent forwarding is disabled, and TCP forwarding is restricted to the local direction so the session can still be used as an SSH jump host without permitting remote port forwarding.
    
    (cherry picked from commit f0502bcec5d513afe81762747269e56c2e4393c0)

--- workshop-images/base-environment/opt/eduk8s/etc/setup.d/02-sshd.sh
+++ workshop-images/base-environment/opt/eduk8s/etc/setup.d/02-sshd.sh
@@ -16,7 +16,6 @@ chmod g-w $HOME
 
 mkdir -p /opt/sshd
 
-ssh-keygen -q -N "" -t dsa -f /opt/sshd/ssh_host_dsa_key
 ssh-keygen -q -N "" -t rsa -b 4096 -f /opt/sshd/ssh_host_rsa_key
 ssh-keygen -q -N "" -t ecdsa -f /opt/sshd/ssh_host_ecdsa_key
 ssh-keygen -q -N "" -t ed25519 -f /opt/sshd/ssh_host_ed25519_key
@@ -26,9 +25,13 @@ Port 2022
 HostKey /opt/sshd/ssh_host_rsa_key
 HostKey /opt/sshd/ssh_host_ecdsa_key
 HostKey /opt/sshd/ssh_host_ed25519_key
-LogLevel DEBUG3
-ChallengeResponseAuthentication no
+LogLevel INFO
+PermitRootLogin no
 PasswordAuthentication no
+KbdInteractiveAuthentication no
+AuthenticationMethods publickey
+AllowTcpForwarding local
+AllowAgentForwarding no
 X11Forwarding no
 PrintMotd no
 PidFile /opt/sshd/sshd.pid
