56555dd951816b258629922532a39985e2145cdb
Jorge Morales Pou <jorgemoralespou@gmail.com>
2026-06-29 12:13:38 +0200

    feat(cli): Update version to one liner and full via flag

--- client-programs/pkg/cmd/project_version_cmd.go
+++ client-programs/pkg/cmd/project_version_cmd.go
@@ -4,26 +4,42 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type ProjectVersionOptions struct {
+	full bool
+}
+
 /*
 Create Cobra command object for displaying Educates version.
 */
 func (p *ProjectInfo) NewProjectVersionCmd() *cobra.Command {
+	var o ProjectVersionOptions
+
 	var c = &cobra.Command{
 		Args:  cobra.NoArgs,
 		Use:   "version",
 		Short: "Display the version of Educates being used",
 		RunE: func(cmd *cobra.Command, _ []string) error {
-			cmd.Println(p.Version)
-			if p.GitCommit != "" {
-				commit := "commit: " + p.GitCommit
-				if p.BuildDate != "" {
-					commit += " (" + p.BuildDate + ")"
+			fullVersion := p.Version
+			if o.full {
+				if p.GitCommit != "" {
+					commit := "commit: " + p.GitCommit
+					if p.BuildDate != "" {
+						commit += " (" + p.BuildDate + ")"
+					}
+					fullVersion += " [" + commit + "]"
 				}
-				cmd.Println(commit)
 			}
+			cmd.Println(fullVersion)
 			return nil
 		},
 	}
+
+	c.Flags().BoolVar(
+		&o.full,
+		"full",
+		false,
+		"full version details",
+	)
 
 	return c
 }
